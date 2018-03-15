64a40c58049b7951146b01ab1dd23dc21cd56445
adityapalmehula <javajiadityapal@gmail.com>
2018-03-15 18:41:30 +0530

    5thcommit

--- src/app/services/json-api.service.ts
+++ src/app/services/json-api.service.ts
@@ -2,10 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-
 import { AppConfig }from '../config/config.constant';
-
-
 
 @Injectable()
 export class JsonApiService {
